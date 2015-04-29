a3cd5cdf6b0358e021cd8f6f3df9f2883510befa
Hans Hillen <hans.hillen@gmail.com>
2015-04-29 17:53:38 +1200

    More setup

---
+++ js/liveregionnotifier.js
@@ -0,0 +1,43 @@
+$(function(){
+	var iChannel = 0;
+	var $lrContainer;
+
+	function clear($channel) {
+		// Remove message
+		$channel.remove();
+	}
+
+	function setupNotifier() {
+		// Set up a live region for feedback
+		$lrContainer = $("#TPG_lRNotifier");
+		if (!$lrContainer.length) {
+			$lrContainer = $("<div></div>").attr('id', 'TPG_lRNotifier').attr('aria-live', 'polite').attr("aria-relevant", "additions").attr("role", "status").addClass("HiddenText").appendTo(document.body);
+		}
+	}
+
+	function notify(strFeedback, selector) {
+		setTimeout(function(){
+            if (selector) {
+                // show text permanently in existing (visible) container
+                $(selector).text(strFeedback);
+                return;
+            }
+			var $channel = $("<div></div>").attr("id", 'channel' + iChannel).text(strFeedback).appendTo($lrContainer)
+			//$lrContainer.text(strFeedback);
+			lrNotifier.iChannel++;
+			if (lrNotifier.iChannel > 1000) {
+				lrNotifier.iChannel = 0;
+			}
+			setTimeout(function(){
+				clear($channel);
+			}, 10000);
+
+		}, 0);
+		
+	}
+	window.lrNotifier = {
+		setupNotifier: setupNotifier,
+		notify: notify
+	};
+});
+
